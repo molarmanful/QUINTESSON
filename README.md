e4a4f2447e7a35bfde431aba31a9d92f0da6ecc0
molarmanful <ben@benpa.ng>
2025-03-14 17:22:20 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# QUINTESSON
+
+A tall, many-faced bitmap megafont to conquer the galaxy with.
---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "A tall, many-faced bitmap megafont to conquer the galaxy with.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    systems.url = "systems";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+    bited-utils.url = "github:molarmanful/bited-utils";
+  };
+
+  outputs =
+    inputs@{ systems, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
+        inputs.bited-utils.flakeModule
+      ];
+      systems = import systems;
+      perSystem =
+        { config, pkgs, ... }:
+        {
+
+          bited-utils = {
+            name = "QUINTESSON";
+            version = builtins.readFile ./VERSION;
+            src = ./.;
+          };
+
+          devshells.default = {
+
+            commands = with pkgs; [
+              {
+                package = nil;
+                category = "lsp";
+              }
+              {
+                package = nixd;
+                category = "lsp";
+              }
+              {
+                package = nixfmt-rfc-style;
+                category = "formatter";
+              }
+              {
+                package = statix;
+                category = "linter";
+              }
+              {
+                package = deadnix;
+                category = "linter";
+              }
+              { package = taplo; }
+              {
+                package = marksman;
+                category = "lsp";
+              }
+              {
+                package = mdformat;
+                category = "formatter";
+              }
+              { package = config.bited-utils.bited-clr; }
+            ];
+
+            packages = with pkgs; [
+              python313Packages.mdformat-gfm
+              python313Packages.mdformat-gfm-alerts
+            ];
+          };
+        };
+    };
+}
